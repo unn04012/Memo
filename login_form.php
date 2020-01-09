a2f9703575e1865de12c8fdf5e2b877f74ef5c04
unn04012 <unn04012@naver.com>
2020-01-10 04:15:04 +0900

    login_form.php first commit

---
+++ login_form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LOGIN FORM</title>
+  <link rel="stylesheet" href="message.css">
+  <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+</head>
+<body>
+  <div class="login-box">
+    <h1>Login</h1>
+    <form action="">
+      <div class="textbox">
+        <i class="fas fa-user"></i>
+        <input type="text" placeholder = "Username" name = "">
+      </div>
+      <div class="textbox">
+        <i class="fas fa-lock"></i>
+        <input type="password" placeholder = "Password" name = "">
+      </div>
+      <input class = "btn" type="button" value = "Sign in">
+    </form>
+  </div>
+</body>
+</html>
